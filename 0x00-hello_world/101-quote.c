7f6d28f5e2c159383e6ecc205361319ee1121016
Joy Wangui <joywangui655@gmail.com>
2023-06-16 06:03:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,5 +6,5 @@
  */
 int main (void)
 {
-	fput("and that piece of art is useful\" - Dora Kor par, 2015-10-19\n", stdout);return (1);
+fput("and that piece of art is useful\" - Dora Kor par, 2015-10-19\n", stdout);return (1);
 }	
