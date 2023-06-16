773fd2f93cd4225add22a7452a62c12a9977380f
Joy Wangui <joywangui655@gmail.com>
2023-06-15 20:01:49 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,4 +7,8 @@
 int main(void)
 {
        printf("Size of a char: %lu byte(s)\n," sizeof(char));
-}       
+      prinf("Size of a int: %lu byte(s)\n," sizeof(int));
+      prinf("Size of a long int: %lu byte(s)\n," sizeof(long int));
+      prinf("Size of a long long int: %lu byte(s)\n," sizeof(long long int));
+      prinf("Size of a float : %lu byte(s)\n," sizeof(float)); return (0)
+}	      
