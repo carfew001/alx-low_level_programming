773a0bd62ed09d93b2a006b7a60264f4da09fee9
Joy Wangui <joywangui655@gmail.com>
2023-06-19 06:13:43 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,6 @@
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
 	char c;
@@ -15,12 +14,12 @@ int main(void)
 	d = 'A';
 	while 
 		(c <=  'z') {
-			putchar (c);
+			putchar(c);
 			c++;
 		}
 	while 
 		(d <= 'z') {
-			putchar (d);
+			putchar(d);
 			d++;
 		}
 	putchar('\n')
