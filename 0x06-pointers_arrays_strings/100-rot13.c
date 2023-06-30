45da38d1ac0a77cbeb190a7e31515ba7f727f906
Joy Wangui <joywangui655@gmail.com>
2023-06-30 02:17:52 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: pointer to string parameter
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqestuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == data1[i])
+			{
+				if (s[j] == datarot[j])
+					break;
+			}
+		}
+	}
+}
