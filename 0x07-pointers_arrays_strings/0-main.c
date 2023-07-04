231cabe43b7da0c87c99658c6e3fc636cbf17843
Joy Wangui <joywangui655@gmail.com>
2023-07-04 01:46:58 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -25,20 +27,4 @@ void simple_print_buffer(char *buffer, unsigned int size)
                 i++;
         }
         printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
 }
